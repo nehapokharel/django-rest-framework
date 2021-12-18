06424249081f1944ab873ef8b0d0cb82c6c7e4a1
Neha Pokharel <nehapokharel008@gmail.com>
2021-12-18 19:04:43 +0545

    model creation

--- sentiment_app/models.py
+++ sentiment_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class ExcelFile(models.Model):
+    file = models.FileField(upload_to='excel')
+
+class AnalyzedFile(models.Model):
+    text = models.TextField()
+    pos = models.FloatField()
+    neg = models.FloatField()
+    neu = models.FloatField()
+    compound = models.FloatField()
+    file = models.ForeignKey(ExcelFile, on_delete=models.CASCADE)
